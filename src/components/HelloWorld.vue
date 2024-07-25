230742c35597005880708fd0a1761f6d45bb91a4
k06181303 <k06181303@gmail.com>
2024-07-26 03:57:32 +0800

    1#	new file:   .gitignore 2#	new file:   README.md 3#	new file:   babel.config.js 4#	new file:   jsconfig.json 5#	new file:   package-lock.json 6#	new file:   package.json 7#	new file:   public/favicon.ico 8#	new file:   public/index.html 9#	new file:   src/App.vue 10#	new file:   src/assets/logo.png 11#	new file:   src/common/base.css 12#	new file:   src/components/HelloWorld.vue 13#	new file:   src/main.js 14#	new file:   vue.config.js

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <p>
+    小滴課堂
+  </p>
+  </div>
+  <el-button type="primary">按鈕</el-button>
+  <el-icon><Search /></el-icon>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<style lang="less" scoped>
+div {
+  p{
+    color: blue;
+  }
+}
+
+</style>
